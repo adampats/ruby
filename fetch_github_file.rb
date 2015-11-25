47ad5fd0687847f35fd8d88f74cbfa8f34e5cc7e
adampats <adamthepatterson@gmail.com>
2015-11-24 16:40:21 -0800

    fixed api endpoint url

--- fetch_github_file.rb
+++ fetch_github_file.rb
@@ -1,28 +1,29 @@
 # Grab a single file from github
-# Requires a token
+# Requires a token - go here: https://github.com/settings/tokens
 
 require 'json'
 require 'rest_client'
 require 'pry-byebug'
 
 # Parameters
-GH_HOST = "github.com"
+GH_HOST = "api.github.com"
 GH_TOKEN = "your_token"
 GH_REPO = "adampats/ruby"
 REPO_FILE = "fetch_github_file.rb"
 OUT_FILE = "fetch_github_file.rb_deleteme"
 
 # Script
-gh_url = "https://#{GH_HOST}/api/v3/repos/#{GH_REPO}/contents/#{REPO_FILE}"
+gh_url = "https://#{GH_HOST}/repos/#{GH_REPO}/contents/#{REPO_FILE}"
 begin
+  print "GET #{gh_url} ..."
   resp = RestClient.get( gh_url,
-	 accept: 'application/vnd.github.v3.raw',
+	  accept: 'application/vnd.github.v3.raw',
 	  Authorization: "token #{GH_TOKEN}" )
+  puts "Done"
 
-  puts "Writing to #{OUT_FILE}..."
+  print "Writing to #{OUT_FILE}..."
   File.write( OUT_FILE, resp )
+  puts "Done"
 rescue => e
-  puts "Error: #{e.message}"
+  puts "Oh noes! An error: #{e.message}"
 end
-
-puts "Done."
